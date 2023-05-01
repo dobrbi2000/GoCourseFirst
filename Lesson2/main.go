4e5d264fd5d7e059f6e3c01d9a7c5d5c8d7b7217
dobrbi2000 <yauhenidabrydzeneu@gmail.com>
2023-05-01 16:02:48 +0300

    Lesson2

---
+++ Lesson2/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var age int
+	// var height int = 186
+
+	// fmt.Println("My age is:", age)
+	// fmt.Println("My height is:", height)
+
+	// var (
+	// 	personName string = "Garry"
+	// 	personAge         = 39
+	// 	personID   int
+	// )
+
+	// // fmt.Printf("Name:%s\nAge:%d\nID:%d\n", personName, personAge, personID)
+
+	a, b := 5, 10
+	c, b, a := 1, 2, 3
+
+	fmt.Println(a, b, c)
+}
